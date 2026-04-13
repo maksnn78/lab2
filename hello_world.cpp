ee4457ac099f8be19fda0c0e5f272106df25450f
maksnn78 <maksnn78@gmail.com>
2026-04-13 16:27:43 +0000

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
+// Program prints greeting message with user input
 int main() {
     std::string name;
     std::cin >> name;
+    // output greeting
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
